4ddcba50bcaa0fd8d68e64770bfb88221506d019
soopdal <soopdal@gmail.com>
2016-07-15 18:15:36 +0900

    예제1 메인
    첫번째 예제, 메인 메소드

---
+++ Ex1_Main.cs
@@ -0,0 +1,13 @@
+﻿//using UnityEngine;
+using System.Collections;
+
+namespace Ex1_Main
+{
+    public class Ex1_Main                                                       // : MonoBehaviour
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World");
+        }
+    }
+}
---
+++ Ex1_Main/ConsoleApplication1/Ex1_Main.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+namespace Ex1_Main
+{
+    public class Ex_1Main                                                       
+    {
+        static void Main(string[] args)                                         // #1 Main은 static 메소드로 한다. #2 Main 메소드는 하나다.
+        {
+            System.Console.WriteLine("Hello, World");                           // "계속하려면 아무키나 누르십시오..."를 나타나게 하려면 Ctrl+F5로 디버깅 없이 실행한다.
+        }
+    }
+}
