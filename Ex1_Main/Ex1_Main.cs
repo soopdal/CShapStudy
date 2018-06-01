361b64992f405755b34b88fa30605f9d638d26e8
bindalbnf <38849719+bindalbnf@users.noreply.github.com>
2018-06-01 12:14:14 +0900

    Ex1_Main에 다음의 주석을 추가한다.
    C#을 공부하기 위해 http://www.csharpstudy.com/ 의 예제 샘플을 만든 것입니다.

--- Ex1_Main/Ex1_Main.cs
+++ Ex1_Main/Ex1_Main.cs
@@ -3,8 +3,8 @@
 
 namespace Ex1_Main
 {
-    public class Ex_1Main                                                       
-    {
+    public class Ex_1Main
+    {                                                                           // C#을 공부하기 위해 http://www.csharpstudy.com/ 의 예제 샘플을 만든 것입니다.
         static void Main(string[] args)                                         // #1 Main은 static 메소드로 한다. #2 Main 메소드는 하나다.
         {
             System.Console.WriteLine("Hello, World");                           // "계속하려면 아무키나 누르십시오..."를 나타나게 하려면 Ctrl+F5로 디버깅 없이 실행한다.
